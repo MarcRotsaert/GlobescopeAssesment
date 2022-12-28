81207cf96e781b475e770fe6500d818925450b05
MarcRotsaert <1065070@hr.nl>
2022-12-28 18:34:17 +0100

    start met routing

--- code/main.py
+++ code/main.py
@@ -1,3 +1,6 @@
+import copy
+
+
 class Node:
     def __init__(self, name):
         self.name = name
@@ -22,9 +25,9 @@ class Train:
         self.node = startnode
         self.distance = 0
 
-    def changeposition(self, nieuwnode):
+    def changeposition(self, nieuwnode, enr=0):
         self.node = nieuwnode
-        self.distance += nieuwnode.edgein[0].weight
+        self.distance += nieuwnode.edgein[enr].weight
         # edgein moet slimmer
 
     def print_distance(self):
@@ -47,17 +50,101 @@ def find_node(sel, nodes):
     return node
 
 
+def routing(beginnode, endnode, nodes):
+    routes = []
+    routes.append([[beginnode], False])
+
+    i = 0  # Route teller
+    node = beginnode
+    # beginnode
+    if node.edgeout == []:
+        routes[0][1] = True
+
+    tempcopy = copy.deepcopy(routes[i])
+    # tempcopy = routes[i].copy()
+    for j in range(len(node.edgeout)):
+        if j < 1:
+            routes[i][0].append(node.edgeout[j])
+        else:
+            routes.append(tempcopy)
+            routes[-1][0].append(node.edgeout[j])
+
+    edgein = routes[i][-1]
+    for no in nodes:
+        if edgein == no.edgein:
+            routes[-1]
+
+    # for in e_out in node.edgeout:
+    #    routes[0].append(node.edgeout[0])
+
+    # routes[0][0].append(beginnode)
+
+    # e_out = beginnode.edgeout[0]
+    # for no in nodes:
+    #     for e_out in no.edgeout:
+    #         if e_out.name == e_in.name:
+    #             routes[0][0].append(no)
+    #             e_out = no.edgein[0]
+    #             break
+
+    # e_out = beginnode.edgeout
+    # node = beginnode
+
+    # for ed in e_out:
+    #     for no in nodes:
+    #         if no.edgein==ed
+    #            break
+    #     node= no
+    #     print(node.name)
+
+    return routes
+
+
 def main():
-    routes = ["AB5", "BC4", "CD8", "DC8", "DE6", "AD5", "CE2", "EB3", "AE7"]
-    dump = decode_edge(routes[0])
+    edgedefs = ["AB5", "BC4", "CD8", "DC8", "DE6", "AD5", "CE2", "EB3", "AE7"]
+    nodescoll = []
+    for temp in edgedefs:
+        dump = decode_edge(temp)
+        e_1 = Edge(dump[0] + dump[1], dump[2])
+        # e_1 = Edge(dump[0] + dump[1], dump[2])
+        # dump = decode_edge(routes[1])
+        # e_2 = Edge(dump[0] + dump[1], dump[2])
+        if len(nodescoll) == 0:
+            n1 = Node(dump[0])
+            n1.add_edgeout(e_1)
+            nodescoll.append(n1)
+            n2 = Node(dump[1])
+            n2.add_edgeout(e_1)
+            nodescoll.append(n2)
+            # nodescoll.append(Node(dump[1]))
+        else:
+            for i, no in enumerate(nodescoll):
+                if no.name == dump[0]:
+                    no.add_edgeout(e_1)
+                    break
+            for i, no in enumerate(nodescoll):
+                if no.name == dump[1]:
+                    no.add_edgein(e_1)
+                    break
+                if i == len(nodescoll) - 1:
+                    no = Node(dump[1])
+                    no.add_edgein(e_1)
+                    nodescoll.append(Node(dump[1]))
+            # print(nodescoll)
+            print(nodescoll)
+            # for i in nodescoll:
+            #    print(i.edgeout)
+            for i in nodescoll:
+                print(i.edgein)
+            print(len(nodescoll))
 
-    e_1 = Edge(dump[0] + dump[1], dump[2])
-    dump = decode_edge(routes[1])
-    e_2 = Edge(dump[0] + dump[1], dump[2])
-
-    nA = Node("A")
-    nB = Node("B")
-    nC = Node("C")
+    # xx
+    myroutes = routing(nodescoll[0], nodescoll[1], nodescoll)
+    print(myroutes)
+    xx
+    # nA = Node("A")
+    # nB = Node("B")
+    # nC = Node("C")
 
     nA.add_edgeout(e_1)
     nB.add_edgein(e_1)
@@ -70,10 +157,14 @@ def main():
     # print(e.edgein)
     # print(e.edgeout)
 
-    trein = Train(nA)
-    trein.changeposition(nB)
-    trein.changeposition(nC)
-    trein.print_distance()
+    if False:
+        trein = Train(nA)
+        trein.changeposition(nB)
+        trein.changeposition(nC)
+        trein.print_distance()
+
+    ro = routing(nA, nC, [nA, nB, nC])
+    print(ro)
 
 
 if __name__ == "__main__":
